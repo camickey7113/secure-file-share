aed4ec4240292dc92faf0cbf2c90373fbd30431a
gjm55 <gjm55@pitt.edu>
2024-10-02 13:58:48 -0400

    gab added token class

--- src/Token.java
+++ src/Token.java
@@ -1,8 +1,28 @@
 public class Token {
     public String username;
     public String password;
-    public boolean userType; // either student or root. if you are a student, you can write, create, or delete a file that is 
-    public boolean read;
-    public boolean ableToCreateUser_or_ableToDeleteUser;
-}
+    public boolean writeCreateOrDeleteFile; // true if you are a student and in your group folder. or, simply if you are
+                                            // root.
+    public boolean manageUsers; // create or delete users. only true if you are root.
+    public String group; // only applies to student users
 
+
+
+    public Token(String username, String password, boolean writeCreateOrDeleteFile, boolean manageUsers, String group) {
+        this.username = username;
+        this.password = password;
+        this.writeCreateOrDeleteFile = writeCreateOrDeleteFile;
+        this.manageUsers = manageUsers;
+
+
+        // set group to null if the user is root. 
+        if (username.equals("root")){
+            this.group = null; // root user, no group 
+        }
+        else{
+            this.group = group; //student 
+        }
+
+    }
+
+}
