77f4d37af50175803390657648f4b032a09745fc
gjm55 <gjm55@pitt.edu>
2024-10-02 13:36:56 -0400

    Added files for specific message types

--- src/Token.java
+++ src/Token.java
@@ -2,7 +2,7 @@ public class Token {
     public String username;
     public String password;
     public boolean userType; // either student or root. if you are a student, you can write, create, or delete a file that is 
-    boolean read;
-    boolean ableToCreateUser_or_ableToDeleteUser;
+    public boolean read;
+    public boolean ableToCreateUser_or_ableToDeleteUser;
 }
 
