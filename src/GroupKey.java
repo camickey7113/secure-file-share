23c459d88a40ffee7239c256bcb89f59de7c246c
Cameron Mickey <camickey7113@gmail.com>
2024-12-03 11:59:07 -0500

    implement GroupKey class

---
+++ src/GroupKey.java
@@ -0,0 +1,47 @@
+import java.security.Security;
+import java.util.ArrayList;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+// VERSION is 0-INDEXED!!!
+public class GroupKey {
+    // list of all keys that have existed for the group, in order of creation
+    ArrayList<byte[]> history;
+
+    public GroupKey() {
+        // initialize history
+        history = new ArrayList<byte[]>();
+        // create first key
+        newVersion();
+    }
+
+    public void newVersion() {
+        history.add(generateKey());
+    }
+
+    public byte[] generateKey() {
+        java.security.Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES", "BC");
+        keyGen.init(256);
+        SecretKey secretKey = keyGen.generateKey();
+        return secretKey.getEncoded();
+    }
+
+    // Returns the most recent gkey
+    public byte[] getCurrentKey() {
+        return history.getLast();
+    }
+
+    // Returns the key specified by the provided version
+    public byte[] getKey(int i) {
+        return history.get(i - 1);
+    }
+
+    // Returns the current version
+    public int getVersion() {
+        return history.size();
+    }
+}
