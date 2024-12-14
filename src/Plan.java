8d6f8d477a349b1a1011c8799c792fb68298ba43
Cameron Mickey <camickey7113@gmail.com>
2024-12-13 19:19:23 -0500

    the plan.

---
+++ src/Plan.java
@@ -0,0 +1,17 @@
+public class Plan {
+    
+    // function to encrypt files
+
+    // function to decrypt files
+
+    // 
+
+    // write groupkeys to a file
+
+    // read groupkeys from a file
+
+    // write versioning HashMap to a file
+
+    // read versioning HashMap from a file
+
+}
